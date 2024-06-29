d3776118d1829eb99af0cfe6ed46682552b1e28c
aditya <adityaedu22@gmail.com>
2024-06-29 20:26:58 +0530

    implement routes using hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,15 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
+    JWT_SECRET: string
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-  return c.text('signup')
-})
-
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('signin')
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('signin')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('signin')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('signin')
-})
-
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('signin')
-})
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app
