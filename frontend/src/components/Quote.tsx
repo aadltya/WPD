4c0f6d6bca3f9b7c6d9eb0849381d5b0e352848b
aditya <adityaedu22@gmail.com>
2024-06-30 13:25:36 +0530

    frontend pages partially

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+    <div className="flex justify-center">
+        <div className="max-w-lg">
+            <div className="text-2xl font-semibold">
+                "One year can change your whole life, just trust the process."
+            </div>
+            <div className="max-w-md text-lg font-medium text-left mt-4">
+                Aditya Swami
+            </div>
+            <div className="max-w-md text-sm font-light text-slate-600">
+                CEO, Medium Inc
+            </div>
+        </div>
+    </div>
+    
+</div>
+}
