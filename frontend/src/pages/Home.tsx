814b5ff30798293aaa7e840e1bd69971c0e164aa
aditya <adityaedu22@gmail.com>
2024-07-19 23:32:48 +0530

    landing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Home = () => {
-    return  <div className="">
-    <div className="max-w-screen-xl">
-        <header className="bg-white shadow-sm">
-            <div className="container w-full px-4 py-4 flex justify-between items-center">
-                <h1 className="text-2xl text-black">Medium</h1>
-                <div className="flex space-x-4">
-                    <Link to={'/signup'} >
-                        <button className="text-white bg-black p-2 px-6 rounded-md">Sign Up</button>
-                    </Link>
-                    <Link to={'/signin'} >
-                        <button className="text-white bg-black p-2 px-6 rounded-md">Sign In</button>
-                    </Link>
-                </div>
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
+  return (
+    <div className="h-screen ">
+      <div className="flex justify-between px-10 py-4">
+        <div className="flex flex-col justify-center">
+            <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+                WPL
+            </h1>
+        </div>
+        <div className="flex space-x-4"> 
+          <Link to="/signup">
+            <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sign up</button>
+          </Link>
+          <Link to="/signin">
+            <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sign in</button>
+          </Link>
+        </div>
+      </div>
+      <div className="h-full flex items-center justify-center pb-80">
+        <div className="flex flex-col" >
+            <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+                Watch People's Lies
+            </h1>
+            <div className="pt-4  " >
+                <Link to="/signin">
+                    <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Get Started</button>
+                </Link>
             </div>
-        </header>
+        </div>
+      </div>
     </div>
-  </div>
-}
+  );
+};
