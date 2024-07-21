72ba7d1f3782d8c052a5470faca86e6e3d3c2b8f
aditya <adityaedu22@gmail.com>
2024-07-21 18:29:24 +0530

    home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -45,20 +45,20 @@ export function Home() {
         };
     }, []);
 
-    const textEnter = () => {
-        setCursorVariant("text");
-    };
+    // const textEnter = () => {
+    //     setCursorVariant("text");
+    // };
 
-    const textLeave = () => {
-        setCursorVariant("default");
-    };
+    // const textLeave = () => {
+    //     setCursorVariant("default");
+    // };
 
     return (
         <div className="">
             <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-stretch antialiased">
                 <div className="flex justify-between px-10 py-4 relative z-10">
                     <div className="flex flex-col justify-center">
-                        <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+                        <h1 className="animate-text cursor-default bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
                             WPL
                         </h1>
                     </div>
@@ -73,7 +73,7 @@ export function Home() {
                 </div>
 
                 <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-                    <div onMouseEnter={textEnter} onMouseLeave={textLeave} className="max-w-2xl mx-auto p-4">
+                    <div className="max-w-2xl mx-auto p-4 cursor-default">
                         <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
                             Watch People Lie
                         </h1>
