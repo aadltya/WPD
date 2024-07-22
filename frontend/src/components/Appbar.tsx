0566b4597ec72573e212308a2dcaeec2f5a3706b
aditya <adityaedu22@gmail.com>
2024-07-22 22:34:17 +0530

    logout

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,4 +1,3 @@
-import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
 export const Appbar = () => {
@@ -13,7 +12,9 @@ export const Appbar = () => {
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Create</button>
             </Link>
 
-            <Avatar size={"big"} name="ad" />
+            <Link to={`/`}>
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Logout</button>
+            </Link>
         </div>
     </div>
 }
