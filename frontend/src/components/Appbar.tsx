b8e6dea3e076bdacdee02d6b32f07409fe892913
aditya <adityaedu22@gmail.com>
2024-07-23 19:06:37 +0530

    logout user

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom"
 
+
+const handleLogout = () => {
+    localStorage.removeItem('token');
+};
+
+
+
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
@@ -13,7 +20,7 @@ export const Appbar = () => {
             </Link>
 
             <Link to={`/`}>
-                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Logout</button>
+                <button onClick={handleLogout} type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Logout</button>
             </Link>
         </div>
     </div>
