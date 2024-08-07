19b61ec541500cd4d40ccad5b433c6f063de147b
aditya <adityaedu22@gmail.com>
2024-08-07 18:15:02 +0530

    f up

--- frontend/src/components/FooterDev.tsx
+++ frontend/src/components/FooterDev.tsx
@@ -16,17 +16,7 @@ export const FooterDev = () =>  {
                 <span className="text-sm font-semibold text-gray-500 dark:text-gray-400">Building in public at <a target="_blank" href="https://twitter.com/aditya_swamii" className="text-blue-500 ml-1" >@aditya_swamii</a></span>
             </div>
             <div className="grid grid-cols-2 gap-8 ">
-                <div>
-                    <h2 className="mb-6 text-sm font-semibold text-gray-900  dark:text-white">Resources I use</h2>
-                    <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                        <li className="mb-4">
-                            <a href="https://ui.aceternity.com/" target="_blank" className="hover:underline">Aceternity UI</a>
-                        </li>
-                        <li>
-                            <a href="https://tailwindcss.com/" target="_blank" className="hover:underline">Tailwind CSS</a>
-                        </li>
-                    </ul>
-                </div>
+                
                 <div>
                     <h2 className="mb-6 text-sm font-semibold text-gray-900 dark:text-white">join me</h2>
                     <ul className="text-gray-500 dark:text-gray-400 font-medium">
