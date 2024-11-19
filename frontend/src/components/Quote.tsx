00e5267c46927bc7a413f91ec9f16462e4523e0e
aadltya <adityaedu22@gmail.com>
2024-11-19 10:15:10 +0530

    quote fix

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,13 +3,13 @@ export const Quote = () => {
     <div className="flex justify-center">
         <div className="max-w-lg">
             <div className="text-2xl font-semibold">
-                "One year can change your whole life, just trust the process."
+                
             </div>
             <div className="max-w-md text-lg font-medium text-left mt-4">
-                - Aditya Swami
+                
             </div>
             <div className="max-w-md text-sm font-light text-slate-600">
-                watch people lie Inc
+                
             </div>
         </div>
     </div>
